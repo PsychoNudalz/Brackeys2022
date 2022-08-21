c1384a37d9f36f4ae2fec73b288eb64d46923710
Anson <anson.aw0618@gmail.com>
2022-08-21 22:28:36 +0100

    Anson: Added selecting in world objects

--- Assets/Anson/Sciprts/PlayerInputController.cs
+++ Assets/Anson/Sciprts/PlayerInputController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
@@ -13,9 +15,23 @@ public class PlayerInputController : MonoBehaviour
     [SerializeField]
     private CharacterControllerScript currentCharacterControllerScript;
 
+    [Header("Mouse Settings")]
+    [SerializeField]
+    private List<String> mouseTags;
+
+    [SerializeField]
+    private LayerMask mouseLayer;
+
+    [SerializeField]
+    private ClickableObject hoverObject;
+
+    [SerializeField]
+    private ClickableObject selectObject;
+
     [Header("Components")]
     [SerializeField]
-    private Transform mainCamera;
+    private Camera mainCamera;
+
     public PlayerController PlayerController
     {
         get => playerController;
@@ -28,13 +44,18 @@ public class PlayerInputController : MonoBehaviour
         set => currentCharacterControllerScript = value;
     }
 
-    
+
     private void Awake()
     {
-        mainCamera = Camera.main.transform;
-        
+        mainCamera = Camera.main;
     }
-    
+
+    private void Update()
+    {
+        RaycastHit raycastHit = RaycastFromCameraToMouse();
+
+    }
+
     public void OnMovement(InputValue context)
     {
         Vector3 moveDir = context.Get<Vector2>();
@@ -42,10 +63,11 @@ public class PlayerInputController : MonoBehaviour
         {
             // float targetAngle = Mathf.Atan2(moveDir.x, moveDir.y) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
             // float angle = Mathf.SmoothDamp(())
-            moveDir = Quaternion.Euler(0f, mainCamera.eulerAngles.y, 0f) * new Vector3(moveDir.x,0,moveDir.y);
+            moveDir = Quaternion.Euler(0f, mainCamera.transform.eulerAngles.y, 0f) *
+                      new Vector3(moveDir.x, 0, moveDir.y);
         }
+
         currentCharacterControllerScript.Move(moveDir);
-        print(moveDir);
     }
 
     public void OnSwitchCharacter(InputValue context)
@@ -54,9 +76,43 @@ public class PlayerInputController : MonoBehaviour
         if (value > 0)
         {
             playerController.NextCharacter();
-        }else if (value < 0)
+        }
+        else if (value < 0)
         {
             playerController.PrevCharacter();
         }
+    }
+
+    public RaycastHit RaycastFromCameraToMouse()
+    {
+        Ray ray = new Ray(mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()),mainCamera.transform.forward);
+
+        Physics.Raycast(ray, out var raycastHit, 1000f, mouseLayer);
+        
+        
+        if (raycastHit.collider && mouseTags.Contains(raycastHit.collider.tag))
+        {
+            ClickableObject temp =  raycastHit.collider.GetComponentInParent<ClickableObject>();
+            if (temp)
+            {
+                hoverObject = temp;
+            }
+            else
+            {
+                hoverObject = null;
+            }
+        }
+        else
+        {
+            hoverObject = null;
+        }
+        // Debug.DrawRay(mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()),mainCamera.transform.forward*1000f);
+
+        return raycastHit;
+    }
+
+    public void OnMouseClick(InputValue context)
+    {
+        selectObject = hoverObject;
     }
 }
