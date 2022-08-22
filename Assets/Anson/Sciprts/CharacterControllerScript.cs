d5d2d0f98d631fa493f19ae8f7a1be13f3bacd88
Anson <anson.aw0618@gmail.com>
2022-08-22 14:28:38 +0100

    Added base Ability.cs

--- Assets/Anson/Sciprts/CharacterControllerScript.cs
+++ Assets/Anson/Sciprts/CharacterControllerScript.cs
@@ -3,13 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AliveEnum
+{
+    Alive,
+    Dead
+}
+
+public enum SuppressionEnum
+{
+    None,
+    Suppressed
+}
+
+
 public class CharacterControllerScript : MonoBehaviour
 {
+
+    [Header("States")]
+    [SerializeField]
+    private AliveEnum aliveEnum;
+
+    [SerializeField]
+    private SuppressionEnum suppressionEnum;
+    
     [Header("Components")]
     [SerializeField]
     private CharacterMovementController characterMovementController;
 
     public CharacterMovementController CharacterMovementController => characterMovementController;
+
+
+    public AliveEnum Alive => aliveEnum;
+
+    public SuppressionEnum Suppression => suppressionEnum;
 
     private void Awake()
     {
