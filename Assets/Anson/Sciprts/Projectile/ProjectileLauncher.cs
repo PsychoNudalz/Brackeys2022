3c8f56d7ed1467bfd57019c6dacf4de6a3c433df
Anson <anson.aw0618@gmail.com>
2022-08-25 00:11:53 +0100

    Anson: ProjectileLauncher Complete, missing setting it as consequence

--- Assets/Anson/Sciprts/Projectile/ProjectileLauncher.cs
+++ Assets/Anson/Sciprts/Projectile/ProjectileLauncher.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ProjectileLauncher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private GameObject projectileGO;
+
+    [SerializeField]
+    private Projectile projectile;
+
+    [SerializeField]
+    private Transform launchPoint;
+
+    [SerializeField]
+    private float shootTime = 1;
+
+    private float lastShotTime = 0;
+
+    [SerializeField]
+    private UnityEvent onShootEvent;
+
+    private void Update()
     {
-        
+        if (Time.time - lastShotTime > shootTime)
+        {
+            OnShoot();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnShoot()
     {
-        
+        lastShotTime = Time.time;
+        projectile = Instantiate(projectileGO,launchPoint.position,launchPoint.rotation).GetComponent<Projectile>();
+        projectile.Launch(launchPoint.forward);
+        onShootEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        OnShoot();
     }
 }
