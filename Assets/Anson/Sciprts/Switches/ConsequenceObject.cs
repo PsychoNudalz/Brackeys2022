7393c2a27e1413083a50e03855de17f669547ff8
Anson <anson.aw0618@gmail.com>
2022-08-27 00:40:52 +0100

    Anson: fixed colouring bug

--- Assets/Anson/Sciprts/Switches/ConsequenceObject.cs
+++ Assets/Anson/Sciprts/Switches/ConsequenceObject.cs
@@ -34,7 +34,7 @@ public class ConsequenceObject : MonoBehaviour
     [ContextMenu("OnUse")]
     private void Awake()
     {
-        FindRenderersInConsequences();
+        // FindRenderersInConsequences();
         if (consequenceState == ConsequenceState.Off)
         {
             OnOff();
@@ -44,24 +44,24 @@ public class ConsequenceObject : MonoBehaviour
             OnOn();
         }
     }
-    
-    public void FindRenderersInConsequences()
-    {
-        List<Renderer> temp = new List<Renderer>(renderers);
-        List<Renderer> newList = new List<Renderer>();
-        foreach (ConsequenceObject consequenceObject in GetComponentsInChildren<ConsequenceObject>())
-        {
-            foreach (Renderer renderer in consequenceObject.Renderers)
-            {
-                if (!temp.Contains(renderer))
-                {
-                    newList.Add(renderer);
-                }
-            }
-        }
-
-        renderers = newList.ToArray();
-    }
+    //
+    // public void FindRenderersInConsequences()
+    // {
+    //     List<Renderer> temp = new List<Renderer>(renderers);
+    //     List<Renderer> newList = new List<Renderer>();
+    //     foreach (ConsequenceObject consequenceObject in GetComponentsInChildren<ConsequenceObject>())
+    //     {
+    //         foreach (Renderer renderer in consequenceObject.Renderers)
+    //         {
+    //             if (!temp.Contains(renderer))
+    //             {
+    //                 newList.Add(renderer);
+    //             }
+    //         }
+    //     }
+    //
+    //     renderers = newList.ToArray();
+    // }
 
     public virtual void OnUse()
     {
