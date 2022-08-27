4c44465f7c3d1f93aaf8e8709104daa43a4ad106
Anson <anson.aw0618@gmail.com>
2022-08-27 16:02:14 +0100

    Anson: Added settings for Low Res and Pixel mode

--- Assets/Anson/Sciprts/UI/PlayerUIController.cs
+++ Assets/Anson/Sciprts/UI/PlayerUIController.cs
@@ -12,8 +12,15 @@ public class PlayerUIController : MonoBehaviour
 
     public PlayerUIInteractOptionController PlayerUIInteractOptionController => playerUIInteractOptionController;
 
+    [Header("Retro Effects")]
+    [SerializeField]
+    private GameObject lowPixelRenderTexture;
+
+    [SerializeField]
+    private Material retroMaterial;
     private void Awake()
     {
+
         if (!current)
         {
             current = this;
@@ -27,7 +34,42 @@ public class PlayerUIController : MonoBehaviour
         {
             playerUIInteractOptionController = GetComponentInChildren<PlayerUIInteractOptionController>();
         }
+        
+        current.retroMaterial.SetFloat("_IsActive",1);
+
     }
+
+    private void OnApplicationQuit()
+    {
+        current.retroMaterial.SetFloat("_IsActive",1);
+
+    }
+
+    public static void SetLowPixel(bool b)
+    {
+        current.lowPixelRenderTexture.SetActive(b);
+    }
+
+    public static void ToggleLowPixel()
+    {
+        current.lowPixelRenderTexture.SetActive(!current.lowPixelRenderTexture.activeSelf);
+
+    }
+
+    public static void ToggleRetroMaterial()
+    {
+        if (current.retroMaterial.GetFloat("_IsActive") >= 1f)
+        {
+            current.retroMaterial.SetFloat("_IsActive",0);
+        }
+        else
+        {
+            current.retroMaterial.SetFloat("_IsActive",1);
+
+        }
+    }
+    
+    
 
     // Start is called before the first frame update
     void Start()
