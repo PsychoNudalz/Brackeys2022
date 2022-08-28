bf6ef1f2487b28be911362a70bc242d73769883d
William <rockateerbusiness@gmail.com>
2022-08-28 01:33:49 +0100

    Will: yeah

--- Assets/Will/Scripts/GameManager.cs
+++ Assets/Will/Scripts/GameManager.cs
@@ -62,6 +62,7 @@ public class GameManager : MonoBehaviour
         if (archerSpawned)
         {
             CharacterManager.GetArcher().SetActive(true);
+            Debug.Log("AS");
         }
         if (mageSpawned)
         {
